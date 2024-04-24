b715b0a2e39adbd72baad787a404c7ca5a88999c
Hitalo de Araujo <hitalofilipe.hitalofilipe@gmail.com>
2024-04-23 23:46:41 -0300

    Removendo espacos em List.ts

--- List.ts
+++ List.ts
@@ -4,29 +4,24 @@ export class List {
   private tipo: Object;
 
   constructor(tipo) {
-
       this.tipo = tipo;
   }
 
   public Add(item) {
-
       this.listaGenerica.push(item);
   }
 
   private _single(lambda, tipo) {
-
       let elemento = this.listaGenerica.filter(lambda)[0];
 
       return elemento as typeof tipo;
   }
 
   public Single(lambda) {
-
       return this._single(lambda, this.tipo);
   }
 
   public _singleOrDefault(lambda, tipo) {
-
       if (this.listaGenerica.length > 0)
           return this.listaGenerica.filter(lambda)[0] as typeof tipo;
       else
@@ -34,22 +29,18 @@ export class List {
   }
 
   public SingleOrDefault(lambda) {
-
       return this._singleOrDefault(lambda, this.tipo);
   }
 
   public First(lambda) {
-
       return this._single(lambda, this.tipo);
   }
 
   public FirstOrDefault(lambda) {
-
       return this._singleOrDefault(lambda, this.tipo);
   }
 
   public _lastOrDefault(lambda, tipo) {
-
       if (this.listaGenerica.length > 0)
           return this.listaGenerica.filter(lambda)[this.listaGenerica.length - 1] as typeof tipo;
       else
@@ -57,23 +48,19 @@ export class List {
   }
 
   public LastOrDefault(lambda) {
-
       return this._lastOrDefault(lambda, this.tipo);
   }
 
   public _last(lambda, tipo) {
-
       if (this.listaGenerica.length > 0)
           return this.listaGenerica.filter(lambda)[this.listaGenerica.length - 1] as typeof tipo;
   }
 
   public Last(lambda) {
-
       return this._last(lambda, this.tipo);
   }
 
   public Select(lambdaElement) {
-
       var listaAux: Array<any> = [];
 
       this._where(item => (item.ativo), this.tipo).forEach(element => {
@@ -84,7 +71,6 @@ export class List {
   }
 
   private _where(lambda, tipo) {
-
       if (this.listaGenerica.length > 0)
           return this.listaGenerica.filter(lambda) as typeof tipo;
       else
@@ -92,32 +78,26 @@ export class List {
   }
 
   public Where(lambda) {
-
       return this._where(lambda, this.tipo);
   }
 
   public IndexAt(item) {
-
     return this.listaGenerica.indexOf(item);
   }
 
   public Contains(item) {
-
     return this.listaGenerica.length > 0 && this.listaGenerica.filter(x => x == item)[0] != undefined;
   }
 
   public Clear() {
-
       this.listaGenerica = [];
   }
 
   public Count() {
-
       return this.listaGenerica.length;
   }
 
   public ToList() {
-
       return this.listaGenerica;
   }
 }
